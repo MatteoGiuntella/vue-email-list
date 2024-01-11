080bda8b7bd39d2547ca30e7a51b7dc30d41480f
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-11 11:26:32 +0100

    esercizio finito con bonus

--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,19 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        list : [],
       }
     },
     methods: {
 
-        }
+        },
+    mounted(){
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((res) => {
+            this.list.push(res.data.response)
+        })
+      }
+    }
   }).mount('#app')
